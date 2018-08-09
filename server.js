5ce72dbfaf73d2e41f298db026c33916e1781ede
FishMissile <fishmissile@gmail.com>
2018-08-09 18:44:23 -0400

    Heroku

--- server.js
+++ server.js
@@ -18,7 +18,15 @@ app.use('/graphql', graphqlHTTP({
   graphiql: true
 }))
 
-app.get('/', (req, res) => res.send('client/src/index'))
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 /* // API calls
 app.get('/api/hello', (req, res) => {
   res.send({ express: testinfo.info1 });
