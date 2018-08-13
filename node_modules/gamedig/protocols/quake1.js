1073192abf8d2f9aa1ad0dd92cbb2cba0d936922
FishMissile <fishmissile@gmail.com>
2018-08-13 19:30:08 -0400

    untracked node_modules

--- node_modules/gamedig/protocols/quake1.js
+++
@@ -1,9 +0,0 @@
-class Quake1 extends require('./quake2') {
-    constructor() {
-        super();
-        this.responseHeader = 'n';
-        this.isQuake1 = true;
-    }
-}
-
-module.exports = Quake1;
